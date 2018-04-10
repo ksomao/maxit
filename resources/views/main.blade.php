2003097319748416b76066beba49117add79b603
Unknown <thomaspoint@gmail.com>
2018-04-10 12:09:30 +0200

    test

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Maxi-it</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}"/>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
